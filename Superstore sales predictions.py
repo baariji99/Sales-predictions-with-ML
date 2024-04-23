4ff72fffd0005857696000e0ae8323e1b139d836
arijit <arijitonnet@gmail.com>
2024-04-23 10:37:03 +0530

    commit

--- Superstore sales predictions.py
+++ Superstore sales predictions.py
@@ -6,3 +6,26 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
+# loading the data from csv file to Pandas DataFrame
+big_mart_data = pd.read_csv('/content/Train.csv')
+# first 5 rows of the dataframe
+big_mart_data.head()
+# number of data points & number of features
+big_mart_data.shape
+# getting some information about thye dataset
+big_mart_data.info()
+# checking for missing values
+big_mart_data.isnull().sum()
+# mean value of "Item_Weight" column
+big_mart_data['Item_Weight'].mean()
+# filling the missing values in "Item_weight column" with "Mean" value
+big_mart_data['Item_Weight'].fillna(big_mart_data['Item_Weight'].mean(), inplace=True)
+# mode of "Outlet_Size" column
+big_mart_data['Outlet_Size'].mode()
+# filling the missing values in "Outlet_Size" column with Mode
+mode_of_Outlet_size = big_mart_data.pivot_table(values='Outlet_Size', columns='Outlet_Type', aggfunc=(lambda x: x.mode()[0]))
+print(mode_of_Outlet_size)
+# checking for missing values
+big_mart_data.isnull().sum()
+#Data Analysis
+big_mart_data.describe()
